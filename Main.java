e936b65b6b82b652d08b8e81c3096b65f72a9f8b
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2025-11-02 00:12:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package version_f.q3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int TResources = 3;
+		int TUsers = 5;
+		ResourceManager rm = new ResourceManager(TResources);
+		
+		for (int i = 1; i <= TUsers; i++) {
+			UserThread userThread = new UserThread(i, rm);
+			userThread.start();
+		}
+	}
+}
